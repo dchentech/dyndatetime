04e2c54d7b9f476bce3b6a4f1cab54b559ccf730
David Chen <mvjome@gmail.com>
2011-04-15 14:49:27 +0800

    Fix lang/zh.js garbled, copied from http://code.google.com/p/dyndatetime/issues/detail?id=22&can=1&q=zh

--- lang/calendar-zh.js
+++ lang/calendar-zh.js
@@ -1,20 +1,20 @@
-/*
+// ** I18N
+
 // Calendar ZH language
 // Author: muziq, <muziq@sina.com>
 // Encoding: GB2312 or GBK
 // Distributed under the same terms as the calendar itself.
-*/
 
 // full day names
 Calendar._DN = new Array
-("������",
- "����һ",
- "���ڶ�",
- "������",
- "������",
- "������",
- "������",
- "������");
+("Sunday",
+ "Monday",
+ "Tuesday",
+ "Wednesday",
+ "Thursday",
+ "Friday",
+ "Saturday",
+ "Sunday");
 
 // Please note that the following array of short day names (and the same goes
 // for short month names, _SMN) isn't absolutely necessary.  We give it here
@@ -30,48 +30,49 @@ Calendar._DN = new Array
 
 // short day names
 Calendar._SDN = new Array
-("��",
- "һ",
- "��",
- "��",
- "��",
- "��",
- "��",
- "��");
+("日",
+ "一",
+ "二",
+ "三",
+ "四",
+ "五",
+ "六",
+ "日");
 
+Calendar._FD = 0;
 // full month names
 Calendar._MN = new Array
-("һ��",
- "����",
- "����",
- "����",
- "����",
- "����",
- "����",
- "����",
- "����",
- "ʮ��",
- "ʮһ��",
- "ʮ����");
+("一月",
+ "二月",
+ "三月",
+ "四月",
+ "五月",
+ "六月",
+ "七月",
+ "八月",
+ "九月",
+ "十月",
+ "十一月",
+ "十二月");
 
 // short month names
 Calendar._SMN = new Array
-("һ��",
- "����",
- "����",
- "����",
- "����",
- "����",
- "����",
- "����",
- "����",
- "ʮ��",
- "ʮһ��",
- "ʮ����");
+("一月",
+ "二月",
+ "三月",
+ "四月",
+ "五月",
+ "六月",
+ "七月",
+ "八月",
+ "九月",
+ "十月",
+ "十一月",
+ "十二月");
 
 // tooltips
 Calendar._TT = {};
-Calendar._TT["INFO"] = "����";
+Calendar._TT["INFO"] = "帮助";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Time Selector\n" +
@@ -79,41 +80,41 @@ Calendar._TT["ABOUT"] =
 "For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
 "Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
 "\n\n" +
-"ѡ������:\n" +
-"- ��� \xab, \xbb ��ťѡ�����\n" +
-"- ��� " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " ��ťѡ���·�\n" +
-"- �������ϰ�ť�ɴӲ˵��п���ѡ����ݻ��·�";
+"选择日期:\n" +
+"- 点击 \xab, \xbb 按钮选择年份\n" +
+"- 点击 " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " 按钮选择月份\n" +
+"- 长按以上按钮可从菜单中快速选择年份或月份";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
-"ѡ��ʱ��:\n" +
-"- ���Сʱ����ӿ�ʹ����ֵ��һ\n" +
-"- ��סShift����Сʱ����ӿ�ʹ����ֵ��һ\n" +
-"- ����϶����ɽ��п���ѡ��";
+"选择时间:\n" +
+"- 点击小时或分钟可使改数值加一\n" +
+"- 按住Shift键点击小时或分钟可使改数值减一\n" +
+"- 点击拖动鼠标可进行快速选择";
 
-Calendar._TT["PREV_YEAR"] = "��һ�� (��ס��˵�)";
-Calendar._TT["PREV_MONTH"] = "��һ�� (��ס��˵�)";
-Calendar._TT["GO_TODAY"] = "ת������";
-Calendar._TT["NEXT_MONTH"] = "��һ�� (��ס��˵�)";
-Calendar._TT["NEXT_YEAR"] = "��һ�� (��ס��˵�)";
-Calendar._TT["SEL_DATE"] = "ѡ������";
-Calendar._TT["DRAG_TO_MOVE"] = "�϶�";
-Calendar._TT["PART_TODAY"] = " (����)";
+Calendar._TT["PREV_YEAR"] = "上一年 (按住出菜单)";
+Calendar._TT["PREV_MONTH"] = "上一月 (按住出菜单)";
+Calendar._TT["GO_TODAY"] = "转到今日";
+Calendar._TT["NEXT_MONTH"] = "下一月 (按住出菜单)";
+Calendar._TT["NEXT_YEAR"] = "下一年 (按住出菜单)";
+Calendar._TT["SEL_DATE"] = "选择日期";
+Calendar._TT["DRAG_TO_MOVE"] = "拖动";
+Calendar._TT["PART_TODAY"] = " (今日)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "�������ʾ%s";
+Calendar._TT["DAY_FIRST"] = "最左边显示%s";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
 // means Monday, etc.
 Calendar._TT["WEEKEND"] = "0,6";
 
-Calendar._TT["CLOSE"] = "�ر�";
-Calendar._TT["TODAY"] = "����";
-Calendar._TT["TIME_PART"] = "(Shift-)��������϶��ı�ֵ";
+Calendar._TT["CLOSE"] = "关闭";
+Calendar._TT["TODAY"] = "今日";
+Calendar._TT["TIME_PART"] = "(Shift-)点击鼠标或拖动改变值";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
-Calendar._TT["TT_DATE_FORMAT"] = "%A, %b %e��";
+Calendar._TT["TT_DATE_FORMAT"] = "%A, %b %e日";
 
-Calendar._TT["WK"] = "��";
-Calendar._TT["TIME"] = "ʱ��:";
+Calendar._TT["WK"] = "周";
+Calendar._TT["TIME"] = "时间:";
